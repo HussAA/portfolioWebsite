69e1649228c16cbad4afd5c842d2e8190733c086
HussAA <mah.hussaina@gmail.com>
2023-05-02 18:53:28 -0400

    coming soon screen

---
+++ src/components/ComingSoon/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <>
+      <div className="overlay">
+        <div className="coming-soon">
+            Coming Soon
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ComingSoon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,72 +9,78 @@ import Education from "../components/Education";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import { Helmet } from "react-helmet";
+import ComingSoon from "../components/ComingSoon";
 const Index = () => {
   return (
     <>
-      <Helmet>
-        <title>Hussain Alnakhli</title>
-      </Helmet>
-      <header>
-        <section>
-          <NavbarIndex />
-        </section>
-        <section>
-          <FrontPage />
-        </section>
-        <section>
-          <Sidebar />
-        </section>
-      </header>
+      <head>
+        <Helmet>
+          <title>Hussain Alnakhli</title>
+        </Helmet>
+      </head>
+      <section>
+        <ComingSoon className="coming-soon-body" />
+      </section>
+      <body>
+        <header>
+          <section>
+            <NavbarIndex />
+          </section>
+          <section>
+            <FrontPage />
+          </section>
+          <section>
+            <Sidebar />
+          </section>
+        </header>
 
-      <main>
-        <section
-          id="About"
-          className="about-fill"
-          style={{
-            minHeight: 400,
-          }}
-        >
-          <AboutMe />
-        </section>
-        <section
-          id="Projects"
-          className="project-fill"
-          style={{
-            minHeight: 600,
-            paddingTop: "80px",
-            
-          }}
-        >
-          <Projects />
-        </section>
-        <section
-          id="Experience"
-          className="experience-fill"
-          style={{
-            minHeight: 600,
-            
-          }}
-        >
-          <Experience />
-        </section>
+        <main>
+          <section
+            id="About"
+            className="about-fill"
+            style={{
+              minHeight: 400,
+            }}
+          >
+            <AboutMe />
+          </section>
+          <section
+            id="Projects"
+            className="project-fill"
+            style={{
+              minHeight: 600,
+              paddingTop: "80px",
+            }}
+          >
+            <Projects />
+          </section>
+          <section
+            id="Experience"
+            className="experience-fill"
+            style={{
+              minHeight: 600,
+            }}
+          >
+            <Experience />
+          </section>
 
-        <section
-          id="Education"
-          className="education-fill"
-          style={{ minHeight: 600, marginBottom: "0px" }}
-        >
-          <Education />
-        </section>
+          <section
+            id="Education"
+            className="education-fill"
+            style={{ minHeight: 600, marginBottom: "0px" }}
+          >
+            <Education />
+          </section>
 
-        <section id="Contact">
-          <Contact />
-        </section>
-      </main>
+          <section id="Contact">
+            <Contact />
+          </section>
+        </main>
 
-      <footer>
-        <Footer/>
-      </footer>
+        <footer>
+          <Footer />
+        </footer>
+      </body>
     </>
   );
 };
