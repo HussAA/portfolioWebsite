ab192ec49d4dec32e4762ecb55721d0999362455
HussAA <mah.hussaina@gmail.com>
2023-05-20 09:14:58 -0400

    template link added to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,72 @@
 import * as React from "react";
 import Logo2 from "../../assets/images/Logo2.png";
+import { styled } from "styled-components";
+
+
+const TemplateSwitch = styled.a`
+  text-align: center;
+  text-decoration: none;
+  font-size: 18px;
+  color: #cfcfcf;
+  display: block;
+  text-shadow: 3px 3px 8px 10px #331f00;
+  transition: 0.2s;
+  &:hover,
+  :focus {
+    color: #6487d3;
+    transform: scale(1, 1);
+    cursor: pointer;
+  }
+`; 
+const TemplateLinkA = styled.a`
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  color: #cfcfcf;
+  display: block;
+  text-shadow: 3px 3px 8px 10px #331f00;
+  transition: 0.2s;
+  &:hover,
+  :focus {
+    color: #6487d3;
+    transform: scale(1, 1);
+    cursor: pointer;
+  }
+`;
+const TemplateLinkB = styled.a`
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  color: #cfcfcf;
+  display: block;
+  text-shadow: 3px 3px 8px 10px #331f00;
+  transition: 0.2s;
+  &:hover,
+  :focus {
+    color: #6487d3;
+    transform: scale(1, 1);
+    cursor: pointer;
+  }
+`;
+
+const TemplatesSide = () => {
+  return (
+    <>
+      <TemplateLinkA href="/portfolio-Template">Portfolio</TemplateLinkA>
+      <hr className="line w-75 m-auto my-3" />
+      <TemplateLinkB href="/CV-Template">CV/Resume</TemplateLinkB>
+    </>
+  );
+};
+
+
 const Sidebar = () => {
+  const [isHidden, setIsHidden] = React.useState(true);
+
+  const handleClick = () => {
+    setIsHidden(false);
+  };
+
   return (
     <>
       <div id="hideside" className="container sidebar">
@@ -45,10 +111,23 @@ const Sidebar = () => {
             </i>
           </a>
           <hr className="line w-100" />
+
+          <TemplateSwitch onClick={handleClick} className=" sidebar-height-m">
+            {isHidden ? (
+              <i className="bi bi-columns">
+                <br />
+                <span>Website Templates</span>
+              </i>
+            ) : (
+              <TemplatesSide />
+            )}
+          </TemplateSwitch>
+          <hr className="line w-100" />
         </div>
       </div>
     </>
   );
 };
+
 
 export default Sidebar;
