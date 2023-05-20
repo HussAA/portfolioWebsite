5e0c6d6cac1c13550cb5804f7447945fe3118599
HussAA <mah.hussaina@gmail.com>
2023-05-20 11:02:56 -0400

    netlify function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,16 +1,43 @@
-exports.handler = async function (event, context) {  
-    switch (event.httpMethod) {
-        /* POST /.netlify/functions/api */
-        case 'POST':
-            return {
-                statusCode: 200,
-                body: JSON.stringify(process.env.SENDGRID_API_KEY ),
-            }
-        /* Fallthrough case */
-        default:
-          return {
-            statusCode: 500,
-            body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE'
-          }
-      }
+const sgMail = require("@sendgrid/mail");
+
+exports.handler = async function (event, context) {
+  switch (event.httpMethod) {
+    /* POST /.netlify/functions/api */
+
+    case "POST":
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+      const { form } = JSON.parse(event.body);
+      const { full_name, email, subject, message } = form;
+
+      const msg = {
+        to: "mah.hussaina@gmail.com", // to your recipient
+        from: {
+          name: `${full_name} `,
+          email: "hussainalnakhli.com@gmail.com", // to your verified sender
+        },
+        subject: `Subject: ${subject} From: ${email}`,
+        text: message,
+      };
+      sgMail
+        .send(msg)
+        .then((response) => {
+          console.log(response[0].statusCode);
+          console.log(response[0].headers);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      console.log(msg);
+      return {
+        statusCode: 200,
+        body: JSON.stringify("Hello"),
+      };
+    /* Fallthrough case */
+    default:
+      return {
+        statusCode: 500,
+        body: "unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE",
+      };
+  }
 };
